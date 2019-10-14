e203e138243e6f38bf215feae824e4ebcf8d3a04
peeweep <peeweep@0x0.ee>
2019-10-15 01:37:00 +0800

    Update resource usage instance

--- src/mywidget.cpp
+++ src/mywidget.cpp
@@ -1,11 +1,12 @@
 #include "mywidget.h"
-#include "./ui_mywidget.h"
-
+#include "ui_mywidget.h"
+#include <QIcon>
 myWidget::myWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::myWidget)
 {
     ui->setupUi(this);
+    this->setWindowIcon(QPixmap(":/Image/QtProject-qtcreator.png"));
 }
 
 myWidget::~myWidget()
